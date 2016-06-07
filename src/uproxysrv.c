5ba18de04ff81407651968703cfef8f5b41fea96
jazzl0ver <root@monitor-1>
2016-06-07 10:17:30 +0000

    1. removed DOS-style line endings (\r) 2. added binding to a specific local ip address

--- src/uproxysrv.c
+++ src/uproxysrv.c
@@ -46,10 +46,10 @@
 
 #define VERSION "0.91"
 
-#define ARGNUM 3
+#define ARGNUM 4
 
 SOCKET ProxySocket;
-struct sockaddr_in ProxySAddr, ClientSAddr, TargetSAddr;
+struct sockaddr_in ProxySAddr, ProxySAddr2, ClientSAddr, TargetSAddr;
 struct sockaddr AnySAddr={AF_INET, {INADDR_ANY}};
 struct ClientList
 {
@@ -110,11 +110,12 @@ int main(int argc, char *argv[])
     time_t last_stat;
     unsigned long nonzero=1;
 
-    printf("UProxy %s - Copyright (C) 2000 Alessandro Staltari", VERSION);
+    printf("UProxy %s - Copyright (C) 2000 Alessandro Staltari\n", VERSION);
     printf("\nDistributed under the terms of the GNU General Public License 2.0\n\n");
     if (argc!=ARGNUM+1)
     {
-        printf("uproxy LocalPort ServerAddress ServerPort\n");
+        printf("uproxy LocalAddress LocalPort ServerAddress ServerPort\n");
+        printf("  use 'any' (w/o quotes) instead of LocalAddress to listen to all interfaces\n");
         exit(EXIT_FAILURE);
     }
 #ifdef WIN32
@@ -123,34 +124,30 @@ int main(int argc, char *argv[])
 #endif
 
     last_stat=time(NULL);
-    ProxySocket=socket(AF_INET, SOCK_DGRAM, 0);
+    ProxySocket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     ioctlsocket ( ProxySocket, FIONBIO, &nonzero);
     memset(&ProxySAddr,0,sizeof(struct sockaddr_in));
     ProxySAddr.sin_family=AF_INET;
-    ProxySAddr.sin_port=htons((unsigned short) atoi(argv[1]));
-    ProxySAddr.sin_addr.s_addr=INADDR_ANY;
+    ProxySAddr.sin_port=htons((unsigned short) atoi(argv[2]));
+    if (strncmp(argv[1],"any",3) == 0)
+	    ProxySAddr.sin_addr.s_addr=INADDR_ANY;
+    else
+    	ProxySAddr.sin_addr.s_addr=inet_addr(argv[1]);
     bind(ProxySocket, (const struct sockaddr *) &ProxySAddr, sizeof(struct sockaddr_in));
 
     TargetSAddr.sin_family=AF_INET;
-    TargetSAddr.sin_port=htons((unsigned short) atoi(argv[3]));
-    TargetSAddr.sin_addr.s_addr=inet_addr(argv[2]);
+    TargetSAddr.sin_port=htons((unsigned short) atoi(argv[4]));
+    TargetSAddr.sin_addr.s_addr=inet_addr(argv[3]);
 
-    printf("\nProxy server started on port %d. Forwarding data to %s:%d\n", atoi(argv[1]), argv[2], atoi(argv[3]));
     while (1)
     {
-#ifdef WIN32          
-        Sleep(1);
-#else
-        sleep(1);
-#endif    
-    
         fd_set r_fdset;
         struct timeval tv;
         struct ClientList *client=Clients;
         int maxfd;
         time_t current_time;
 
-        tv.tv_sec=1;
+        tv.tv_sec=100;
         tv.tv_usec=0;
         FD_ZERO(&r_fdset);
         FD_SET(ProxySocket, &r_fdset);
@@ -163,7 +160,7 @@ int main(int argc, char *argv[])
             {
                 FD_SET(c->sock, &r_fdset);
                 if (c->sock > maxfd) maxfd=c->sock;
-                c=c->next;
+	                c=c->next;
             }
         }
         select(maxfd+1, &r_fdset, NULL, NULL, &tv);
@@ -171,70 +168,96 @@ int main(int argc, char *argv[])
         if ((current_time-last_stat)>=10)
         {
             printf("in %8d out %8d\n", bytes_in-old_bytes_in, bytes_out-old_bytes_out);
+            fflush(stdout);
             old_bytes_out=bytes_out;
             old_bytes_in=bytes_in;
             last_stat=current_time;
         }
-        if ((recbuflen=recvfrom(ProxySocket, recbuf, 2048, 0, (struct sockaddr *) &fromaddr, &fromaddrlen))>=0)
+
+	fromaddrlen = sizeof(struct sockaddr);
+        if ((recbuflen = recvfrom(ProxySocket, recbuf, 2048, 0, (struct sockaddr *) &fromaddr, &fromaddrlen))>=0)
         {
             struct ClientList *c=Clients;
-            
-            while (c!=NULL)
+            while (c != NULL)
             {
                 if ((c->saddr.sin_addr.s_addr!=fromaddr.sin_addr.s_addr)
-                    || (c->saddr.sin_port!=fromaddr.sin_port))
-                c=c->next;
-                else break;
+							|| (c->saddr.sin_port!=fromaddr.sin_port))
+				{
+					c = c->next;
+				}
+                else 
+					break;
             }
             if (c==NULL)
             {
+				int reuse = 1;
+				int reuselen = sizeof(reuse);
+                int res = 0;
+
                 c=malloc(sizeof(struct ClientList));
                 c->next=Clients;
                 Clients=c;
                 memcpy(&(c->saddr), &fromaddr, sizeof(struct sockaddr_in));
                 printf("%s:%d Connected.\n", inet_ntoa(c->saddr.sin_addr), ntohs(c->saddr.sin_port));
-                c->sock=socket(AF_INET, SOCK_DGRAM, 0);
+            fflush(stdout);
+                c->sock=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
                 ioctlsocket (c->sock, FIONBIO, &nonzero);
-                bind(c->sock, (const struct sockaddr *) &AnySAddr, sizeof(struct sockaddr_in));
+	        memset(&ProxySAddr2,0,sizeof(struct sockaddr_in));
+	        ProxySAddr2.sin_family=AF_INET;
+	        ProxySAddr2.sin_port=33336;
+		ProxySAddr2.sin_addr.s_addr=ProxySAddr.sin_addr.s_addr;
+//	        setsockopt(c->sock, SOL_SOCKET, SO_REUSEADDR, (const char*)&reuse, reuselen);
+  	        res=bind(c->sock, (const struct sockaddr *) &ProxySAddr2, sizeof(struct sockaddr_in));
+		if (res != 0) printf("error\n");
+            fflush(stdout);
             }
-            //printf("OUT: %s:%d\n", inet_ntoa(fromaddr.sin_addr), ntohs(fromaddr.sin_port));
+
+            printf("<- %s:%d\n", inet_ntoa(fromaddr.sin_addr), ntohs(fromaddr.sin_port)); 
+            fflush(stdout);
             sendto(c->sock, recbuf, recbuflen, 0, (const struct sockaddr *) &TargetSAddr,
                    sizeof(struct sockaddr_in));
             c->last=current_time;
             bytes_out+=recbuflen;
         }
-        client=Clients;
+
+		client=Clients;
         while(client!=NULL)
         {
+			fromaddrlen = sizeof(struct sockaddr);
             if ((recbuflen=recvfrom(client->sock, recbuf, 2048, 0, (struct sockaddr *) &fromaddr, &fromaddrlen))>=0)
             {
-                //printf("IN: %s:%d\n", inet_ntoa(fromaddr.sin_addr), ntohs(fromaddr.sin_port));
+                printf("<- %s:%d\n", inet_ntoa(fromaddr.sin_addr), ntohs(fromaddr.sin_port)); 
+            fflush(stdout);
                 sendto(ProxySocket, recbuf, recbuflen, 0, (const struct sockaddr *) &(client->saddr),
                        sizeof(struct sockaddr_in));
-                bytes_in+=recbuflen;
+                bytes_in += recbuflen;
             }
-            else if ((current_time-client->last)>30)
-            {
-                struct ClientList *c=Clients;
+            else
+			{
+				if ((current_time-client->last)>30)
+				{
+					struct ClientList *c=Clients;
 
-                closesocket(client->sock);
-                printf("%s:%d Disconnected.\n", inet_ntoa(client->saddr.sin_addr), ntohs(client->saddr.sin_port));
-                if (client!=Clients)
-                {
-                    while (c->next!=client)
-                        c=c->next;
-                    c->next=c->next->next;
-                    free(client);
-                    client=c;
-                }
-                else
-                {
-                    Clients=client->next;
-                    free(client);
-                    client=Clients;
-                    continue;
-                }
-            }
+					closesocket(client->sock);
+					printf("%s:%d Disconnected.\n", inet_ntoa(client->saddr.sin_addr), ntohs(client->saddr.sin_port));
+            fflush(stdout);
+					if (client!=Clients)
+					{
+						while (c->next!=client)
+							c=c->next;
+						c->next=c->next->next;
+						free(client);
+						client=c;
+					}
+					else
+					{
+						Clients=client->next;
+						free(client);
+						client=Clients;
+						continue;
+					}
+				}
+			}
             client=client->next;
         }
     }
